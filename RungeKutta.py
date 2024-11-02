7a07679512d07c78a8ccaa6a791dc798268d73e5
Obi-Wan <85265883+WanSuyu@users.noreply.github.com>
2024-11-02 11:32:30 +0800

    RungeKutta.py

--- RungeKutta.py
+++ RungeKutta.py
@@ -13,8 +13,8 @@ def RK_solve(x0,y0,z0,n,h):
     l2 = g(x0+h/2,y0+(h*k1)/2,z0+(h*l1)/2,n)
     k3 = f(x0+h/2,y0+(h*k2)/2,z0+(h*l2)/2)
     l3 = g(x0+h/2,y0+(h*k2)/2,z0+(h*l2)/2,n)
-    k4 = f(x0+h/2,y0+(h*k3)/2,z0+(h*l3)/2)
-    l4 = g(x0+h/2,y0+(h*k1)/2,z0+(h*l3)/2,n)
+    k4 = f(x0+h,y0+h*k3,z0+h*l3)
+    l4 = g(x0+h,y0+h*k3,z0+h*l3,n)
     x1 = x0+h
     y1 = y0+(h/6)*(k1+2*k2+2*k3+k4)
     z1 = z0+(h/6)*(l1+2*l2+2*l3+l4)
